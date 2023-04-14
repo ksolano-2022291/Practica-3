ad63415abb3f5cba4fffa3ba90b52ec6ddfc54fe
ksolano-2022291 <128552006+ksolano-2022291@users.noreply.github.com>
2023-04-14 14:34:20 -0600

    Add files via upload

---
+++ PRACTICA_3.2_KEVIN/PRACTICA_3_KEVIN.ino
@@ -0,0 +1,121 @@
+/*
+   Fundacion Kinal
+   Centro educativo tecnico laboral Kinal
+   Quinto perito
+   Quinto electronica
+   Codigo Tecnico:EB5AV 
+   Curso: Taller de electronica digital y reparacion de computadoras I
+   Proyecto: Luces Autofantastico.
+   Dev: Kevin Audiel Solano Torres
+   Fecha: 14 de Abril
+*/
+
+#define LED1  2 // definir para led 1 conectado en pin 2
+#define LED2  3 // definir para led 2 conectado en pin 3
+#define LED3  4 // definir para led 3 conectado en pin 4
+#define LED4  5 // definir para led 4 conectado en pin 5
+#define LED5  6 // definir para led 5 conectado en pin 6
+#define LED6  7 // definir para led 6 conectado en pin 7
+#define LED7  8 // definir para led 7 conectado en pin 8
+#define LED8  9 // definir para led 8 conectado en pin 9
+#define LED9  10 // definir para led 9 conectado en pin 10
+#define LED10 11 // definir para led 10 conectado en pin 11
+void setup() {
+  pinMode(LED1,OUTPUT);//pin 2 como salida para led1
+  pinMode(LED2,OUTPUT);//pin 2 como salida para led1
+  pinMode(LED3,OUTPUT);//pin 2 como salida para led1
+  pinMode(LED4,OUTPUT);//pin 2 como salida para led1
+  pinMode(LED5,OUTPUT);//pin 2 como salida para led1
+  pinMode(LED6,OUTPUT);//pin 2 como salida para led1
+  pinMode(LED7,OUTPUT);//pin 2 como salida para led1
+  pinMode(LED8,OUTPUT);//pin 2 como salida para led1
+  pinMode(LED9,OUTPUT);//pin 2 como salida para led1
+  pinMode(LED10,OUTPUT);//pin 2 como salida para led1
+  
+  
+
+}
+
+void loop() {
+  digitalWrite(LED1,HIGH);
+  delay(50);
+  digitalWrite(LED1,LOW);
+  delay(50);
+  digitalWrite(LED2,HIGH);
+  delay(50);
+  digitalWrite(LED2,LOW);
+  delay(50);
+  digitalWrite(LED3,HIGH);
+  delay(50);
+  digitalWrite(LED3,LOW);
+  delay(50);
+  digitalWrite(LED4,HIGH);
+  delay(50);
+  digitalWrite(LED4,LOW);
+  delay(50);
+  digitalWrite(LED5,HIGH);
+  delay(50);
+  digitalWrite(LED5,LOW);
+  delay(50);
+  digitalWrite(LED6,HIGH);
+  delay(50);
+  digitalWrite(LED6,LOW);
+  delay(50);
+  digitalWrite(LED7,HIGH);
+  delay(50);
+  digitalWrite(LED7,LOW);
+  delay(50);
+  digitalWrite(LED8,HIGH);
+  delay(50);
+  digitalWrite(LED8,LOW);
+  delay(50);
+  digitalWrite(LED9,HIGH);
+  delay(50);
+  digitalWrite(LED9,LOW);
+  delay(50);
+  digitalWrite(LED10,HIGH);
+  delay(50);
+  digitalWrite(LED10,LOW);
+  delay(50);
+
+digitalWrite(LED10,HIGH);
+  delay(50);
+  digitalWrite(LED10,LOW);
+  delay(50);
+  digitalWrite(LED9,HIGH);
+  delay(50);
+  digitalWrite(LED9,LOW);
+  delay(50);
+  digitalWrite(LED8,HIGH);
+  delay(50);
+  digitalWrite(LED8,LOW);
+  delay(50);
+  digitalWrite(LED7,HIGH);
+  delay(50);
+  digitalWrite(LED7,LOW);
+  delay(50);
+  digitalWrite(LED6,HIGH);
+  delay(50);
+  digitalWrite(LED6,LOW);
+  delay(50);
+  digitalWrite(LED5,HIGH);
+  delay(50);
+  digitalWrite(LED5,LOW);
+  delay(50);
+  digitalWrite(LED4,HIGH);
+  delay(50);
+  digitalWrite(LED4,LOW);
+  delay(50);
+  digitalWrite(LED3,HIGH);
+  delay(50);
+  digitalWrite(LED3,LOW);
+  delay(50);
+  digitalWrite(LED2,HIGH);
+  delay(50);
+  digitalWrite(LED2,LOW);
+  delay(50);
+  digitalWrite(LED1,HIGH);
+  delay(50);
+  digitalWrite(LED1,LOW);
+  delay(50);
+}
